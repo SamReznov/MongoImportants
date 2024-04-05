4171d63c7680c4ccca45ebfe40f101a041c83ce2
SamReznov <87170525+SamReznov@users.noreply.github.com>
2024-04-05 11:44:46 +0530

    Create mongoSchema.js

---
+++ mongoSchema.js
@@ -0,0 +1,56 @@
+//this is for creating Schema
+db.createCollection("nonFiction",{
+    validator:{
+        $jsonSchema:{
+            required:['name','price'],
+            properties:{
+                name:{
+                    bsonType:'string',
+                    description:'must be a string and required'
+                },
+                price:{
+                    bsonType:'number',
+                    description:'must be a number and required'
+                }
+            }
+        }
+    },
+    validationAction:'error'
+})
+//this is for modifing schema 
+db.runCommand({ 
+    callMod:'nonFiction',
+    validator:{
+        $jsonSchema:{
+            required:['name','price','authors'],
+            properties:{
+                name:{
+                    bsonType:'string',
+                    description:'must be a string and required'
+                },
+                price:{
+                    bsonType:'number',
+                    description:'must be a number and required'
+                },
+                authors:{
+                    bsonType:'array',
+                    description:'must be a array and required',
+                    items:{
+                        bsonType:'object',
+                        required:['name','email'],
+                        properties:{
+                            name:{
+                                bsonType:'string'
+                            },
+                            email:{
+                                bsonType:'string'
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    },
+    validationAction:'error'
+})
